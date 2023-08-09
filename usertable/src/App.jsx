f110020ff1f32e07b7d8811084f1eff9a5c36b3f
souvik3950 <bhattacharyasouvik39@gmail.com>
2023-08-09 19:41:18 +0530

    Initial Commit and Created One Redux Slice.

---
+++ usertable/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/NavBar";
+import UserDetails from "./components/UserDetails";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <UserDetails />
+    </>
+  );
+};
+
+export default App;
